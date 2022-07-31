389532698b374c3700ea2ee8d41c40791c936680
HasnatSN <hasnat.kontakt@gmail.com>
2022-07-31 14:09:45 +0200

    latest v, will continue on laptop

--- src/modules/weatherdata.js
+++ src/modules/weatherdata.js
@@ -1,12 +1,12 @@
 let getWeatherData = new Promise((resolve, reject) => {
   resolve(
     fetch(
-      "https://api.openweathermap.org/data/2.5/weather?q=Vienna,CA&appid=ef6e090d0fed450ba07f747031ada8e4"
+      "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric&appid=ef6e090d0fed450ba07f747031ada8e4"
     ).then((response) => {
       if (response.ok) {
         return response.json();
       }
-      throw new Error("Something went wrong.");
+      throw new Error("Something went wrong. -HSN");
     })
   );
 });
@@ -14,11 +14,6 @@ let getWeatherData = new Promise((resolve, reject) => {
 getWeatherData
   .then((data) => {
     console.log(data);
-  })
-  .catch((error) => {
-    console.log(error);
   });
 
 export { getWeatherData };
-
-// https://api.openweathermap.org/data/2.5/weather?q=Vienna,CA&appid=ef6e090d0fed450ba07f747031ada8e4
